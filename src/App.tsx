2e3025b6929e2b3ba30d1f514543cb98e891027e
Bruno Netto <brunonettomac@outlook.com>
2022-11-25 21:32:17 -0300

    feat(header page): criação da primeira versão do header da landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { Container, Row, Col } from 'react-grid-system'
 import { globalStyles } from './styles/stitches.config'
-import { Header, Title } from './styles'
+import { Header, Nav, HeaderContent } from './styles'
+
+import logoUiboost from './assets/logo-uiboost.svg'
+import phoneIllustration from './assets/phone-illustration.svg'
+import arrowDown from './assets/arrow-down.svg'
+
+import ButtonStore from './components/ButtonStore'
+
 
 function App() {
   globalStyles()
@@ -8,10 +15,40 @@ function App() {
   return (
     <>
       <Header>
-        <Container>
-          <Row debug>
-            <Col debug>
-              <Title>Home Page</Title>
+        <Container style={{ height: '100%' }}>
+          <Row style={{ height: '100%' }}>
+            <Col style={{ height: '100%' , display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+              <Nav>
+                <img src={logoUiboost} alt="Logo uiboost" />
+
+                <div>
+                  <a href="/#">Sobre o curso</a>
+                  <a href="/#">Depoimentos</a>
+                </div>
+
+                <a href="/#">Dúvidas? Fale conosco ✌</a>
+              </Nav>
+
+              <HeaderContent>
+                <div className="content">
+                  <h3>Exclusivo para alunos uiBoost</h3>
+                  <h2>Estude design até em outra <span>galáxia</span></h2>
+                  <p>Baixe o app do uiBoost e tenha acesso a todo o conteúdo do curso uiBoost na palma da sua mão, mesmo estando offline. Aprenda de qualquer lugar e a qualquer hora.</p>
+
+                  <div className="buttons">
+                    <ButtonStore variant='apple' />
+                    <ButtonStore variant='google' />
+                  </div>
+                </div>
+
+                <div className="image">
+                  <img src={phoneIllustration} alt="Phone illustration" />
+                </div>
+              </HeaderContent>
+
+              <div style={{ textAlign: 'center' }}>
+                <img src={arrowDown} alt="Arrow Bottom" style={{ marginBottom: '10px' }} />
+              </div>
             </Col>
           </Row>
         </Container>
